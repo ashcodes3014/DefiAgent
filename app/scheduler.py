8715d12338c015f0f12b00b0d7eb1ee2ea8547e1
ashcodes3014 <aashishkumarak7065@gmail.com>
2025-08-18 02:03:05 +0530

    Update scheduler.py

--- app/scheduler.py
+++ app/scheduler.py
@@ -2,6 +2,7 @@ from firebase_config import fs
 from dataFetcher import get_coin_features
 from llm_agent import get_llm_action
 import time
+import pytz
 from datetime import datetime
 
 symbol_to_id = {
@@ -48,7 +49,8 @@ def process_user(user: dict):
 
         time.sleep(5)
 
-    results['Updates']['last_updated'] = datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S UTC")
+    india = pytz.timezone("Asia/Kolkata")
+    results['Updates']['last_updated'] = datetime.now(india).strftime("%Y-%m-%d %H:%M:%S %Z")
 
     fs.collection("USERS").document(uid) \
         .collection("wallets").document(address) \
